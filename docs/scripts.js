ced701ca418421ad7192fa97d7cab8e881310d37
Sai Krishna Kuchi <124647422+krishna-k7@users.noreply.github.com>
2024-08-28 15:26:02 +0900

    [skip actions] Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        document.querySelectorAll('.webpage').forEach(webpage => {
+            webpage.classList.toggle('dark-mode');
+        });
+        document.querySelector('header').classList.toggle('dark-mode');
+        document.querySelector('footer').classList.toggle('dark-mode');
+    });
+
     const webpages = document.querySelectorAll('.webpage');
     webpages.forEach((webpage, index) => {
         setTimeout(() => {
